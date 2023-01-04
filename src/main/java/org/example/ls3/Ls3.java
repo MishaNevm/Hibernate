0b59989c135499e31ae3a1c45bd0fba9f494bf71
Misha <ggvbgg@list.ru>
2023-01-04 21:22:43 +0300

    hql read, update, delete

--- src/main/java/org/example/ls3/Ls3.java
+++ src/main/java/org/example/ls3/Ls3.java
@@ -5,21 +5,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
 public class Ls3 {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
-        try{
+        try {
             session.beginTransaction();
-            Person person = new Person(21, "Test2");
-            session.save(person);
-            System.out.println(person.getId());
-            person = session.get(Person.class, 5);
-            person.setName("Test Name3");
+            session.createQuery("delete from Person where age>10").executeUpdate();
             session.getTransaction().commit();
-            System.out.println("Hello");
         } finally {
             sessionFactory.close();
         }
